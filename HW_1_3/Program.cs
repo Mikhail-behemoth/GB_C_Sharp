de66fe8893893ae36e35c2d6d4250e446ae9a2dd
Mikhail <rednex05@yandex.ru>
2022-08-10 01:06:56 +0300

    1

---
+++ HW_1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
---
+++ HW_1_4/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = 0;
+Console.WriteLine("Введите число");
+n = Convert.ToInt32(Console.ReadLine());
+if (n > 0)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write( i + " ");
+        }
+        else
+        {
+            Console.Write("");
+        }
+    }
+}
+else
+{
+        for (int i = n; i <= 1; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write( i + " ");
+        }
+        else
+        {
+            Console.Write("");
+        }
+    }
+}
