89eef409d46c23807756154976d769f62b65be69
Mikhail <rednex05@yandex.ru>
2022-08-10 23:37:25 +0300

    1

---
+++ HW_2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = new Random().Next(1, 1000000);
+Console.WriteLine(a);
+if (a < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    while (a > 1000)
+    {
+        a = a / 10;
+    }
+    Console.WriteLine(a % 10);
+}
---
+++ HW_2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = new Random().Next(1, 8);
+Console.WriteLine(a);
+if (a < 6)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("да");
+}
