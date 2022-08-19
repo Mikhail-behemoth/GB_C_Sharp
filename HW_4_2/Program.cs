02dbda0e7dec6b412f47ad328e17c7dc86186d13
Mikhail <rednex05@yandex.ru>
2022-08-19 11:02:47 +0300

    1

---
+++ HW_4_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int a1 = Math.Abs(a);
+int sum_a = 0;
+while (Math.Abs(a) > 0)
+{
+    a1 = Math.Abs(a) % 10;
+    sum_a = sum_a + a1;
+    a = a / 10;
+}
+Console.WriteLine($"сумма чисел числа = {sum_a}");
---
+++ HW_4_3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите восемь чисел через пробел");
+string[] nums_strings = Console.ReadLine().Split();
+int[] nums = new int[nums_strings.Length];
+if (nums_strings.Length > 8)
+{
+    for(int i = 0; i < 8; i++)
+    {
+    nums[i] = Convert.ToInt32(nums_strings[i]);
+    Console.Write(nums[i] + " ");
+    }
+}
+else
+{
+    for(int i = 0; i < nums_strings.Length; i++)
+    {
+    nums[i] = Convert.ToInt32(nums_strings[i]);
+    Console.Write(nums[i] + " ");
+    } 
+}
