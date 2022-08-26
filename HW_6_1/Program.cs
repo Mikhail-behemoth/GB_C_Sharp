a0407ccdb2bebccceb5a5f9e7fa9e125aec045c9
Mikhail <rednex05@yandex.ru>
2022-08-26 04:39:02 +0300

    1

---
+++ HW_6_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите числа через пробел");
+string[] nums_string = Console.ReadLine().Split();
+
+void CountNaturalNumbers (string [] str)
+{
+int[] arr = new int[str.Length];
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = Convert.ToInt32(str[i]);
+}
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+ {
+    if (arr[i] > 0)
+    {
+     count ++;
+    }
+ }
+ Console.WriteLine($"количество чисел больше нуля равно {count}");
+}
+
+CountNaturalNumbers(nums_string);
---
+++ HW_6_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите b1");
+int B1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1");
+int K1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+int B2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+int K2 = Convert.ToInt32(Console.ReadLine());
+
+void intersectionPointCoordinates (int b1, int k1, int b2, int k2)
+{
+double x = ((double)(b2-b1)/(k1-k2));
+double y = k1*x + b1;
+Console.WriteLine($"({x}, {y})");
+}
+
+intersectionPointCoordinates(B1, K1, B2, K2);
